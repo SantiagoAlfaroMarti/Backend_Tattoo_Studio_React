00dc24e93a1dd2f5b8ecaefe47ae9b48d9730b81
unknown <santiagoalfamar@gmail.com>
2024-07-09 16:34:19 +0200

    feat: roles crud and appointments crud

--- src/server.ts
+++ src/server.ts
@@ -1,70 +1,68 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
 import express from 'express';
-import { createService, deleteServiceById, updateServiceById } from './controllers/services.controller';
-import { createUser, deleteUserById, updateUserById } from './controllers/users.controller';
-import { createAppointment, deleteAppointmentById, updateAppointmentById } from './controllers/appointments.controller';
+import 'dotenv/config'
 import { AppDataSource } from './database/db';
-import { createRole, deleteRoleById, updateRoleById } from './controllers/roles.controller';
+import { changeUserRole, deleteUserById, getAllUsers, getUserByEmail, getUserProfile, modifyUserProfile } from './controllers/users.controller';
+import { register, userLogIn } from './controllers/auth.controller';
+import { createService, deleteService, getAllServices, updateSerivce } from './controllers/services.controller';
+import { auth } from './middlewares/auth';
+import { isAdmin } from './middlewares/isAdmin';
+import { createAppointment, deleteAppointment, findAppointmendById, showMyAppointments, updateAppointment } from './controllers/appointments.controller';
+import { createRole, getRoles, updateRole } from './controllers/roles.controller';
 
 const app = express();
-
-// MIDDLEWARE
 app.use(express.json())
 
-const PORT = process.env.PORT || 4000;
-
-app.get('/healthy', (req, res) => {
-    res.status(200).json(
-        {
-     success: true,
-     message: "Server is healthy"
-    }
-)
-})
-
-
-// Services CRUD
-app.get('/services', (req, res) => {
-    res.send('GET ALL SERVICES')
-})
-app.post('/services', createService)
-app.put('/services/:id', updateServiceById);
-app.delete('/services/:id', deleteServiceById);
-
-// Appointments CRUD
-app.get('/appointments', (req, res) => {
-    res.send('GET ALL APPOINTMENTS')
-})
-app.post('/appointments', createAppointment);
-app.put('/appointments/:id', updateAppointmentById);
-app.delete('/appointments/:id', deleteAppointmentById);
-
-// Users CRUD
-app.get('/users', (req, res) => {
-    res.send('GET ALL USERS')
-})
-app.post('/users', createUser);
-app.put('/users/:id', updateUserById);
-app.delete('/users/:id', deleteUserById);
-
-// Roles CRUD
-app.get('/roles', (req, res) => {
-    res.send('GET ALL USERS')
-})
-app.post('/roles', createRole);
-app.put('/roles/:id', updateRoleById);
-app.delete('/roles/:id', deleteRoleById);
-
+const PORT = process.env.port || 4000;
 
 AppDataSource.initialize()
     .then(() => {
-    console.log('Database connected');
-    app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+        console.log('Database connected');
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
+        })
     })
-})
-.catch(error => {
-console.log(error)
-})
+    .catch(error => {
+        console.log(error)
+    })
+
+
+
+// AUTHENTICATION CRUD
+
+app.post('/api/auth/register', register)       // to create new user  - pass to Thunder > body > email + password
+app.post('/api/auth/login', userLogIn)   //to log in to your account   - pass to Thunder > auth > your token hash
+
+
+// USERS CRUD
+
+app.get('/api/users', auth, isAdmin, getAllUsers)       //To show all users in our DB in admin POV         - pass to Thunder > auth > your token hash
+app.get('/api/users/profile',  auth, getUserProfile)             //To see user profile as user POV          - pass to Thunder > auth > your token hash
+app.put('/api/users/profile', auth, modifyUserProfile)             // To modify(update) user profile as user POV    - pass to Thunder > auth > your token hash + body > info to be updated ex. email
+app.get('/api/users/:email', auth, isAdmin, getUserByEmail)         // to get user by mail
+app.delete('/api/users/:id', auth, isAdmin, deleteUserById)        //to eliminate User finded by ID
+app.put('/api/users/:id/role', auth, isAdmin, changeUserRole)      // to change User role finded by ID
+
+
+//APPOINTMENTS CRUD
+
+app.post('/api/appointments/create', auth, createAppointment)          //Create new appointment           - pass to Thunder > auth > your token hash
+app.put('/api/appointments/change', auth, updateAppointment)          //Update an appointment           - pass to Thunder > auth > your token hash
+app.get('/api/appointments/scheduled', auth, showMyAppointments)     //Show all my appointments        - pass to Thunder > auth > your token hash
+app.delete('/api/appointments/delete', auth, deleteAppointment)      //to delete selected appoitnment
+app.get('/api/appointments/:id', auth, findAppointmendById)         //Show an appointment by ID                - pass to Thunder > auth > your token hash
+
+
+// SERVICES CRUD
+
+app.get('/api/services', getAllServices)                            // to see all the services
+app.post('/api/services', auth, isAdmin, createService)             //to create a service
+app.put('/api/services/:id', auth, isAdmin, updateSerivce)          // to update a service by its ID
+app.delete('/api/services/:id', auth, isAdmin, deleteService)        // to delete a service by its ID
+
+
+// ROLES CRUD
+
+app.get('/api/roles', auth, isAdmin, getRoles)                     // to see all roles
+app.post('/api/roles/create', auth, isAdmin, createRole)           //to make new role
+app.put('/api/roles/update/:id', auth, isAdmin, updateRole)      //to update role by its ID
+app.delete('/api/roles/delete', auth, isAdmin)                  //to delete a role by its ID 

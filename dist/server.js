64d13c0cc56223dec2abafdd7b49aeab8779a85d
unknown <santiagoalfamar@gmail.com>
2024-07-11 21:48:00 +0200

    update: delete commented line that I will not use for the server

--- dist/server.js
+++ dist/server.js
@@ -8,7 +8,6 @@ const app = (0, express_1.default)();
 app.use(express_1.default.json());
 const PORT = process.env.port || 5000;
 app.get('/healthy', (req, res) => {
-    //    res.send('Server is healthy')
     res.status(200).json({
         success: true,
         message: "Server is healthy"
